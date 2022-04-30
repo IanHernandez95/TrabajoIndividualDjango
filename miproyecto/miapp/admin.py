b21b52119bd6ff0151472f10c748bc3bca3f18df
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-04-29 21:23:46 -0400

    se agrega modelo profesor

--- miproyecto/miapp/admin.py
+++ miproyecto/miapp/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Alumno)
+
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido',)
+
+admin.site.register(models.Alumno, AlumnoAdmin)
+
+
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'fecha_contratacion',)
+    list_filter=('fecha_contratacion',)
+
+
+admin.site.register(models.Profesor, ProfesorAdmin)
--- miproyecto/miapp/models.py
+++ miproyecto/miapp/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Alumno(models.Model):
-    nombre = models.CharField(max_length=45,verbose_name='Ingrese Nombre del Alumno')
-    apellido = models.CharField(max_length=45,verbose_name='Ingrese Apellido del Alumno')
-    edad = models.IntegerField(verbose_name='Ingreses edad del Alumno')
-    email = models.EmailField(verbose_name='Ingrese Correo del Alumno')
+    nombre = models.CharField(max_length=45,)
+    apellido = models.CharField(max_length=45,)
+    edad = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=45,)
+    apellido = models.CharField(max_length=45,)
+    edad = models.IntegerField()
+    email = models.EmailField()
+    fecha_contratacion = models.DateField()
 
     def __str__(self):
         return f'{self.nombre} {self.apellido}'
